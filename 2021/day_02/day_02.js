ca6ff34ff6c4defaa17c2b30044629273b2af658
aanand3 <as5265@columbia.edu>
2021-12-02 14:36:54 -0600

    day 2 refactors

--- 2021/day_02/day_02.js
+++ 2021/day_02/day_02.js
@@ -1,26 +1,59 @@
-const fs = require('fs');
-const input =
-    fs.readFileSync(process.stdin.fd)
-        .toString()
-        .split('\n')
+const date = new Date().getDate();
 
-let horizontal_position = 0;
-let depth = 0;
+const fs = require("fs");
+const test_input = fs.readFileSync(`./day_0${date}_test.txt`)
+    .toString()
+    .split("\n");
 
-for (let i = 0; i < input.length; i++) {
-    const [direction, amount] = input[i].split(" ")
-    switch (direction) {
-        case "forward":
-            horizontal_position += +amount;
-            break;
-        case "up":
-            depth -= +amount;
-            break;
-        case "down":
-            depth += +amount;
-            break;
+const actual_input = fs.readFileSync(`./day_0${date}.txt`)
+    .toString()
+    .split("\n");
+
+function part1(input) {
+    let horizontal_position = 0;
+    let depth = 0;
+    for (let i = 0; i < input.length; i++) {
+        const [direction, amount] = input[i].split(" ")
+        switch (direction) {
+            case "forward":
+                horizontal_position += +amount;
+                break;
+            case "up":
+                depth -= +amount;
+                break;
+            case "down":
+                depth += +amount;
+                break;
+        }
     }
+    return horizontal_position * depth
 }
 
-console.log("part 1: ", horizontal_position*depth)
+function part2(input) {
+    let horizontal_position = 0;
+    let depth = 0;
+    let aim = 0;
+    for (let i = 0; i < input.length; i++) {
+        const [direction, amount] = input[i].split(" ")
+        switch (direction) {
+            case "forward":
+                horizontal_position += +amount;
+                depth += (+amount * aim);
+                break;
+            case "up":
+                aim -= +amount;
+                break;
+            case "down":
+                aim += +amount;
+                break;
+        }
+    }
+    return horizontal_position * depth
+}
+
+console.assert(part1(test_input) === 150)
+console.assert(part2(test_input) === 900)
+
+console.log("part 1: ", part1(actual_input))
+console.log("part 2: ", part2(actual_input))
 
