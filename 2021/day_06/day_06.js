9c39c3057c8eb85b033d6122797b7ed95e9e8d01
aanand3 <as5265@columbia.edu>
2021-12-06 10:39:25 -0600

    day 6 solutions

--- 2021/day_06/day_06.js
+++ 2021/day_06/day_06.js
@@ -1,4 +1,4 @@
-const date = new Date().getDate() ;
+const date = new Date().getDate();
 
 const fs = require("fs");
 const test_input = fs.readFileSync(`./day_0${date}_test.txt`)
@@ -11,9 +11,8 @@ const actual_input = fs.readFileSync(`./day_0${date}.txt`)
     .split(",")
     .map(num => parseInt(num));
 
-function part1(input) {
+function part1(input, numDays) {
     let fishes = input
-    let numDays = 18
     for (let i = 0; i < numDays; i++) {
         let tempArray = []
         for (const fish of fishes) {
@@ -27,19 +26,17 @@ function part1(input) {
             }
         }
         fishes = tempArray
-        console.log(i, ":", fishes)
     }
     return fishes.length
 }
 
-function part1_alternate(input) {
-    let numDays = 18
+function part1_alternate(input, numDays) {
     let total = 0;
     for (let i = 1; i < 6; i++) {
         let countOfThisNumber = input.filter(num => num === i).length
-        let familySize = numberOfAllChildren(i, numDays)
+        let familySize = 1 + numberOfAllChildren(i, numDays)
 
-        console.log(familySize, countOfThisNumber)
+        // console.log('family size, count: ', familySize, countOfThisNumber)
         total += familySize * countOfThisNumber
     }
     return total;
@@ -47,42 +44,30 @@ function part1_alternate(input) {
 
 function numberOfAllChildren(startVal, daysRemaining) {
     console.log(startVal, daysRemaining)
-    if (daysRemaining < startVal) return 0; // base case
+    if (daysRemaining <= startVal) return 1; // base case
     else {
-        let my_kids = Math.floor((daysRemaining - startVal)/6) + 1
+        let my_kids = Math.floor((daysRemaining - 1 - startVal) / 7) + 1
         console.log('my kids: ', my_kids)
-        return 1 + my_kids + numberOfAllChildren(8, daysRemaining - startVal - 1 )
+        return 1 + my_kids + numberOfAllChildren(8, daysRemaining - 1 - startVal)
     }
 }
 
-function part2(input) {
+function part2(input, numDays) {
     // let fishes = input.sort(function (a, b) {  return a - b;  });
-
-    let sum = 0;
-
-    for (let i = 1; i < 2; i++) {
-        let loneFishArray = [i]
+    let counts = new Array(9).fill(0);
+    for (let i = 1; i < 6; i++) {
         let countOfThisNumber = input.filter(num => num === i).length
-
-        for (let i = 0; i < 256; i++) {
-            let tempArray = []
-            for (const fish of loneFishArray) {
-                switch (fish) {
-                    case 0:
-                        tempArray.push(6)
-                        tempArray.push(8)
-                        break;
-                    default:
-                        tempArray.push(fish - 1)
-                }
-            }
-            loneFishArray = tempArray
-        }
-
-        sum += (loneFishArray.length * countOfThisNumber)
+        counts[i] = countOfThisNumber
     }
 
-    return sum
+    console.log(counts)
+    for (let i = 0; i < numDays; i++) {
+        let numZeros = counts.shift()
+        counts[6] += numZeros // add to the 6's
+        counts.push(numZeros)
+    }
+
+    return counts.reduce((a, b) =>a+b);
 }
 
 // console.log(part1(test_input))
@@ -90,13 +75,5 @@ function part2(input) {
 // console.log("part 1: ", part1(actual_input))
 
 // console.log(part2(test_input))
-// console.assert(part2(test_input) === 26984457539)
-// console.log("part 2: ", part2(actual_input))
-
-// console.log(numDirectChildren(3, 13))
-
-let testVal = 1
-console.log(part1([testVal]))
-// console.log(part1_alternate(test_input))
-console.log('--------')
-console.log(numberOfAllChildren(testVal, 18))
+console.assert(part2(test_input, 256) === 26984457539)
+console.log("part 2: ", part2(actual_input, 256))
